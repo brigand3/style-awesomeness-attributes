d6dcf4ce1f33088ba02fd7a7ee318234352cbe7c
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-30 08:19:51 +0900

    Author:hjyun Use Theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import styled, { createGlobalStyle, css } from 'styled-components';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from './Theme';
 
 createGlobalStyle`
   body{
@@ -8,27 +9,30 @@ createGlobalStyle`
   }
 `;
 
-const awesomeCard = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
+const Card = styled.div`
+  background-color: ${props => props.theme.mainColor};
 `;
 
-const Input = styled.input.attrs({
-  required: true
-})`
-  border: none;
-  //border-radius: 5px;
-  ${awesomeCard}
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Input placeholder="Hello" />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
